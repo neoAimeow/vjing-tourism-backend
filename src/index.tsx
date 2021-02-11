dc30d201013ef588e3aba949d097e9aba5dd2977
zhaoyi.w <a18814888787@gmail.com>
2021-02-12 01:01:25 +0800

    add antd layout

--- src/index.tsx
+++ src/index.tsx
@@ -1,57 +1,93 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import { Layout, Menu } from "antd";
-import { UserOutlined, VideoCameraOutlined } from "@ant-design/icons";
+import {
+    MenuUnfoldOutlined,
+    MenuFoldOutlined,
+    UserOutlined,
+    VideoCameraOutlined,
+    UploadOutlined,
+} from "@ant-design/icons";
+
+import "./index.css";
 import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
 
-const { Header, Content, Footer, Sider } = Layout;
+const { Header, Sider, Content, Footer } = Layout;
+
+class App extends React.Component {
+    state = {
+        collapsed: false,
+    };
+
+    toggle = () => {
+        this.setState({
+            collapsed: !this.state.collapsed,
+        });
+    };
+
+    render() {
+        return (
+            <Layout>
+                <Sider
+                    trigger={null}
+                    collapsible
+                    collapsed={this.state.collapsed}
+                >
+                    <div className="logo" />
+                    <Menu
+                        theme="dark"
+                        mode="inline"
+                        defaultSelectedKeys={["1"]}
+                    >
+                        <Menu.Item key="1" icon={<UserOutlined />}>
+                            nav 1123
+                        </Menu.Item>
+                        <Menu.Item key="2" icon={<VideoCameraOutlined />}>
+                            nav 2
+                        </Menu.Item>
+                        <Menu.Item key="3" icon={<UploadOutlined />}>
+                            nav 3
+                        </Menu.Item>
+                    </Menu>
+                </Sider>
+                <Layout className="site-layout">
+                    <Header
+                        className="site-layout-background"
+                        style={{ padding: 0 }}
+                    >
+                        {React.createElement(
+                            this.state.collapsed
+                                ? MenuUnfoldOutlined
+                                : MenuFoldOutlined,
+                            {
+                                className: "trigger",
+                                onClick: this.toggle,
+                            }
+                        )}
+                    </Header>
+                    <Content
+                        className="site-layout-background"
+                        style={{
+                            margin: "24px 16px",
+                            padding: 24,
+                            minHeight: 280,
+                        }}
+                    >
+                        Content
+                    </Content>
+                    <Footer style={{ textAlign: "center" }}>
+                        Ant Design ©2018 Created by Ant UED
+                    </Footer>
+                </Layout>
+            </Layout>
+        );
+    }
+}
 
 ReactDOM.render(
     <React.StrictMode>
-        <Layout>
-            <Sider
-                breakpoint="lg"
-                collapsedWidth="0"
-                onBreakpoint={(broken) => {
-                    console.log(broken);
-                }}
-                onCollapse={(collapsed, type) => {
-                    console.log(collapsed, type);
-                }}
-            >
-                <div className="logo" />
-                <Menu theme="dark" mode="inline" defaultSelectedKeys={["4"]}>
-                    <Menu.Item key="1" icon={<UserOutlined />}>
-                        nav 1
-                    </Menu.Item>
-                    <Menu.Item key="2" icon={<VideoCameraOutlined />}>
-                        nav 2
-                    </Menu.Item>
-                    <Menu.Item key="4" icon={<UserOutlined />}>
-                        nav 4
-                    </Menu.Item>
-                </Menu>
-            </Sider>
-            <Layout>
-                <Header
-                    className="site-layout-sub-header-background"
-                    style={{ padding: 0 }}
-                />
-                <Content style={{ margin: "24px 16px 0" }}>
-                    <div
-                        className="site-layout-background"
-                        style={{ padding: 24, minHeight: 360 }}
-                    >
-                        content
-                    </div>
-                </Content>
-                <Footer style={{ textAlign: "center" }}>
-                    Ant Design ©2018 Created by Ant UED
-                </Footer>
-            </Layout>
-        </Layout>
+        <App />
     </React.StrictMode>,
     document.getElementById("root")
 );
