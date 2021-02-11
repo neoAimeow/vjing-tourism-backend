7c374bac10b435df7ea236f344209b38fcb70e99
zhaoyi.w <a18814888787@gmail.com>
2021-02-11 15:56:37 +0800

    add prettier

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,79 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Layout, Menu } from "antd";
+import {
+    AppstoreOutlined,
+    BarChartOutlined,
+    CloudOutlined,
+    ShopOutlined,
+    TeamOutlined,
+    UserOutlined,
+    UploadOutlined,
+    VideoCameraOutlined,
+} from "@ant-design/icons";
+
+const { Header, Content, Footer, Sider } = Layout;
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Layout>
+            <Sider
+                style={{
+                    overflow: "auto",
+                    height: "100vh",
+                    position: "fixed",
+                    left: 0,
+                }}
+            >
+                <div className="logo" />
+                <Menu theme="dark" mode="inline" defaultSelectedKeys={["4"]}>
+                    <Menu.Item key="1" icon={<UserOutlined />}>
+                        nav 1
+                    </Menu.Item>
+                    <Menu.Item key="2" icon={<VideoCameraOutlined />}>
+                        nav 2
+                    </Menu.Item>
+                    <Menu.Item key="3" icon={<UploadOutlined />}>
+                        nav 3
+                    </Menu.Item>
+                    <Menu.Item key="4" icon={<BarChartOutlined />}>
+                        nav 4
+                    </Menu.Item>
+                    <Menu.Item key="5" icon={<CloudOutlined />}>
+                        nav 5
+                    </Menu.Item>
+                    <Menu.Item key="6" icon={<AppstoreOutlined />}>
+                        nav 6
+                    </Menu.Item>
+                    <Menu.Item key="7" icon={<TeamOutlined />}>
+                        nav 7
+                    </Menu.Item>
+                    <Menu.Item key="8" icon={<ShopOutlined />}>
+                        nav 8
+                    </Menu.Item>
+                </Menu>
+            </Sider>
+            <Layout className="site-layout" style={{ marginLeft: 200 }}>
+                <Header
+                    className="site-layout-background"
+                    style={{ padding: 0 }}
+                />
+                <Content style={{ margin: "24px 16px 0", overflow: "initial" }}>
+                    <div
+                        className="site-layout-background"
+                        style={{ padding: 24, textAlign: "center" }}
+                    ></div>
+                </Content>
+                <Footer style={{ textAlign: "center" }}>
+                    Ant Design ©2018 Created by Ant UED
+                </Footer>
+            </Layout>
+        </Layout>
+    </React.StrictMode>,
+    document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
