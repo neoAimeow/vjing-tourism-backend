88202899aa0d9fc7d8c60a0b0fad3bc65b2bccb6
wuzhaoyi02 <wuzhaoyi02@meituan.com>
2021-07-09 01:52:02 +0800

    feat: react router method

--- src/pages/ScenicRegion/components/CreateScenicRegion/index.tsx
+++ src/pages/ScenicRegion/components/CreateScenicRegion/index.tsx
@@ -1,10 +1,42 @@
+import { Button, Descriptions, Form, Input, PageHeader, Select } from "antd";
 import React from "react";
+import { useHistory } from "react-router-dom";
 
 interface Props {}
 
 const CreateScenicRegion = (props: Props) => {
     console.warn(`create`);
-    return <div> hello test page test</div>;
+    const history = useHistory();
+    return (
+        <div>
+            <PageHeader
+                ghost={false}
+                onBack={() => history.goBack()}
+                title="创建景区"
+                subTitle="景区名字"
+            />
+            <Form name="control-hooks">
+                <Form.Item
+                    name="note"
+                    label="Note"
+                    rules={[
+                        {
+                            required: true,
+                        },
+                    ]}
+                >
+                    <Input />
+                </Form.Item>
+
+                <Form.Item>
+                    <Button type="primary" htmlType="submit">
+                        提交
+                    </Button>
+                    <Button htmlType="button">重置</Button>
+                </Form.Item>
+            </Form>
+        </div>
+    );
 };
 
 export default CreateScenicRegion;
